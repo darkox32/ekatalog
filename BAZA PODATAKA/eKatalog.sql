1cf462b7c27ce9d0d754f4fd6eac8ab30ea214c6
Jana <milanovic.jana12@gmail.com>
2020-05-15 18:05:57 +0200

    Prilagodjen formatu

--- BAZA PODATAKA/eKatalog.sql
+++ BAZA PODATAKA/eKatalog.sql
@@ -23,14 +23,16 @@ CREATE TABLE IF NOT EXISTS `administrator` (
   `password_hash` varchar(128) NOT NULL DEFAULT '0',
   PRIMARY KEY (`administrator_id`) USING BTREE,
   UNIQUE KEY `uq_admin_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table ekatalog.administrator: ~3 rows (approximately)
 /*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
 INSERT INTO `administrator` (`administrator_id`, `username`, `password_hash`) VALUES
 	(1, 'Admin1', 'C82D0A3CD48A7EBD80ED61B1B3169B9CA5C818E35ACF8E87145E163A1302C281F51780040FC2B31EB5933AEC322E15C2F967348411466D1E250AB136F128F092'),
 	(2, 'admn2', '0u25y15029ohifadh\\'),
-	(3, 'pperic', 'tajnaLozinka');
+	(3, 'pperic', 'tajnaLozinka'),
+	(6, 'Noviadmin', 'admin'),
+	(7, 'Jovo', 'Jovo');
 /*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
 
 -- Dumping structure for table ekatalog.category
@@ -72,10 +74,10 @@ INSERT INTO `network` (`network_id`, `name`) VALUES
 -- Dumping structure for table ekatalog.phone
 CREATE TABLE IF NOT EXISTS `phone` (
   `phone_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `category_id` int(10) unsigned NOT NULL DEFAULT 0,
   `name` varchar(128) NOT NULL DEFAULT '',
-  `os` enum('Android','iOS','Windows','Blackberry') DEFAULT NULL,
+  `category_id` int(10) unsigned NOT NULL DEFAULT 0,
   `description` mediumtext DEFAULT NULL,
+  `os` enum('Android','iOS','Windows','Blackberry') DEFAULT NULL,
   `ram_size` int(64) NOT NULL DEFAULT 0,
   `storage_size` int(64) NOT NULL DEFAULT 0,
   `screen_size` int(64) NOT NULL DEFAULT 0,
@@ -83,13 +85,16 @@ CREATE TABLE IF NOT EXISTS `phone` (
   PRIMARY KEY (`phone_id`) USING BTREE,
   KEY `fk_phone_category_id` (`category_id`),
   CONSTRAINT `fk_phone_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table ekatalog.phone: ~1 rows (approximately)
+-- Dumping data for table ekatalog.phone: ~4 rows (approximately)
 /*!40000 ALTER TABLE `phone` DISABLE KEYS */;
-INSERT INTO `phone` (`phone_id`, `category_id`, `name`, `os`, `description`, `ram_size`, `storage_size`, `screen_size`, `created_at`) VALUES
-	(6, 1, 'Apple X', 'iOS', 'SuperFon', 64, 128, 12, '2020-05-09 15:30:45'),
-	(8, 4, 'Samsung A70', 'Android', 'MojFonhue', 128, 246, 22, '2020-05-09 19:47:06');
+INSERT INTO `phone` (`phone_id`, `name`, `category_id`, `description`, `os`, `ram_size`, `storage_size`, `screen_size`, `created_at`) VALUES
+	(6, 'Apple X', 1, 'SuperFon', 'iOS', 64, 128, 12, '2020-05-09 15:30:45'),
+	(8, 'Samsung A70', 4, 'MojFonhue', 'Android', 128, 246, 22, '2020-05-09 19:47:06'),
+	(9, 'Samsung A150', 3, 'Kratak opis...', NULL, 32, 10, 15, '2020-05-12 13:40:53'),
+	(10, 'Apple3', 4, 'Kratak opis..121212321423543gsdgsdg.', NULL, 32, 10, 15, '2020-05-12 13:42:30'),
+	(11, 'Appleff$@$!$@!$3555', 4, 'Kratak opis..121212321423543gsdgsdg.', NULL, 32, 10, 15, '2020-05-15 16:18:26');
 /*!40000 ALTER TABLE `phone` ENABLE KEYS */;
 
 -- Dumping structure for table ekatalog.phone_network
@@ -103,13 +108,16 @@ CREATE TABLE IF NOT EXISTS `phone_network` (
   KEY `fk_phone_network_network_id` (`network_id`),
   CONSTRAINT `fk_phone_network_network_id` FOREIGN KEY (`network_id`) REFERENCES `network` (`network_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_phone_network_phone_id` FOREIGN KEY (`phone_id`) REFERENCES `phone` (`phone_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table ekatalog.phone_network: ~2 rows (approximately)
+-- Dumping data for table ekatalog.phone_network: ~5 rows (approximately)
 /*!40000 ALTER TABLE `phone_network` DISABLE KEYS */;
 INSERT INTO `phone_network` (`phone_network_id`, `phone_id`, `network_id`, `band`) VALUES
 	(4, 6, 3, 'LTE 1'),
-	(5, 6, 1, 'TRL1');
+	(5, 6, 1, 'TRL1'),
+	(6, 9, 1, 'LTE 15005'),
+	(7, 10, 1, 'LTE 15005'),
+	(8, 11, 1, 'LTE 15005');
 /*!40000 ALTER TABLE `phone_network` ENABLE KEYS */;
 
 -- Dumping structure for table ekatalog.phone_price
@@ -121,13 +129,16 @@ CREATE TABLE IF NOT EXISTS `phone_price` (
   PRIMARY KEY (`phone_price_id`) USING BTREE,
   KEY `fk_phone_price_phone_id` (`phone_id`) USING BTREE,
   CONSTRAINT `fk_phone_price_phone_id` FOREIGN KEY (`phone_id`) REFERENCES `phone` (`phone_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table ekatalog.phone_price: ~2 rows (approximately)
+-- Dumping data for table ekatalog.phone_price: ~5 rows (approximately)
 /*!40000 ALTER TABLE `phone_price` DISABLE KEYS */;
 INSERT INTO `phone_price` (`phone_price_id`, `phone_id`, `price`, `created_at`) VALUES
 	(1, 6, 65.00, '2020-05-09 19:09:21'),
-	(4, 6, 55.00, '2020-05-09 19:09:34');
+	(4, 6, 55.00, '2020-05-09 19:09:34'),
+	(5, 9, 50.00, '2020-05-12 13:40:53'),
+	(6, 10, 950.90, '2020-05-12 13:42:30'),
+	(7, 11, 950.90, '2020-05-15 16:18:26');
 /*!40000 ALTER TABLE `phone_price` ENABLE KEYS */;
 
 -- Dumping structure for table ekatalog.photo
@@ -141,7 +152,7 @@ CREATE TABLE IF NOT EXISTS `photo` (
   CONSTRAINT `FK_photo_phone` FOREIGN KEY (`phone_id`) REFERENCES `phone` (`phone_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table ekatalog.photo: ~1 rows (approximately)
+-- Dumping data for table ekatalog.photo: ~0 rows (approximately)
 /*!40000 ALTER TABLE `photo` DISABLE KEYS */;
 INSERT INTO `photo` (`photo_id`, `phone_id`, `image_path`) VALUES
 	(1, 6, 'img/sr/pck');
